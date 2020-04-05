a469e8a06e1a1e83b0d1ed05c16178f2ded3509b
Your Name <bpunyak007@gmail.com>
2020-04-05 12:41:24 -0400

    scrollbars on divs hidden

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let aboutButton = document.querySelector('#aboutBtn')
+let placesButton = document.querySelector('#placesBtn')
+let foodBtn = document.querySelector('#foodBtn')
+let entertainBtn = document.querySelector('#entertainBtn')
+
+let aboutSection = document.querySelector('#about')
+let placesSection = document.querySelector('#places')
+let foodSection = document.querySelector('#about')
+let entertainSection = document.querySelector('#about')
+
+aboutButton.addEventListener('click', event => {
+    
+})
