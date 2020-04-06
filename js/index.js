3ec7f81adb490374d09451d93df743386bad786b
Tracy Tran <tran-tracy@hotmail.com>
2020-04-06 08:22:29 -0400

    fade-in

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ let infiniteScroll = (event) => {
 
   if (winY >= maxY) {
     document.querySelector('#content').innerHTML += `
-    <section>
+    <section class="fade-in">
     <article class="article-txt">
         <h1 class="article-title">Munich</h1>
         <section id="about" class="about">
